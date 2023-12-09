22bbd8a0bbee1246a969d24ef1266b19729a33d5
FloncDev <floncdev@gmail.com>
2023-12-09 02:15:19 +0000

    :construction: Some part 2 work for day 7
    Code really janky just uh please ignore it :)

--- 2023/07/main.py
+++ 2023/07/main.py
@@ -12,9 +12,34 @@ class Deck:
         self.cards = [i for i in self.line[0]]
         self.bet = int(self.line[1])
 
-        self.get_strength(self.cards)
+        strength_return = self.get_strength(self.cards)
+        self.strength = strength_return[0]
+        self.counted = strength_return[1]
 
-    def get_strength(self, cards):
+    def convert_Js(self):
+        joker_count = self.counted.get("J", 0)
+
+        if joker_count == 0:
+            return
+
+        # Change jokers to highest best cards
+        for card in self.counted.keys():
+            if card == "J":
+                continue
+
+            del self.counted["J"]
+            self.counted[card] = self.counted[card] + joker_count
+            break
+
+        joker_cards = []
+        for card, amount in self.counted.items():
+            [joker_cards.append(card) for i in range(amount)]
+
+        self.cards = joker_cards
+
+        self.strength, _ = self.get_strength(self.cards)
+
+    def count_cards(self, cards):
         counted = {}
         for card in cards:
             if card in counted:
@@ -23,27 +48,32 @@ class Deck:
             counted[card] = cards.count(card)
 
         counted = dict(sorted(counted.items(), key=lambda i: i[1])[::-1])
+        return counted
+
+    def get_strength(self, cards):
+        counted = self.count_cards(cards)
         keys = list(counted.values())
+        strength = 0
 
         if keys[0] == 5:
-            self.strength = 0
+            strength = 0
         elif keys[0] == 4:
-            self.strength = 1
+            strength = 1
         elif keys[0] == 3 and keys[1] == 2:
-            self.strength = 2
+            strength = 2
         elif keys[0] == 3:
-            self.strength = 3
+            strength = 3
         elif keys[0] == 2 and keys[1] == 2:
-            self.strength = 4
+            strength = 4
         elif keys[0] == 2:
-            self.strength = 5
+            strength = 5
         else:
-            self.strength = 6
+            strength = 6
 
-        self.counted = counted
+        return strength, counted
 
     def __str__(self):
-        return f"{''.join(self.cards)}({self.strength}) {self.bet} {self.counted}"
+        return f"{''.join(self.cards)}({self.strength}) {self.bet}"
 
     def __repr__(self) -> str:
         return self.__str__()
@@ -62,10 +92,18 @@ class Deck:
             return rankings.index(self_card) > rankings.index(other_card)
 
 
-cards = [Deck(i.strip()) for i in stdin.readlines()]
+decks = [Deck(i.strip()) for i in stdin.readlines()]
 
 winnings = 0
-for index, card in enumerate(sorted(cards)):
+for index, card in enumerate(sorted(decks)):
     winnings += card.bet * (index + 1)
 
+rankings = [i for i in "J23456789TQKA"]
+[i.convert_Js() for i in decks]
+
+joker_winnings = 0
+for index, card in enumerate(sorted(decks)):
+    joker_winnings += card.bet * (index + 1)
+
 print(winnings)
+print(joker_winnings, decks)
